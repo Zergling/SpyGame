667855d8a3f82d68fa00f442efc6b3385d1aa089
Hive <death1681@yandex.ru>
2018-06-11 23:32:44 +0300

    - агент и немного енумов

--- SpyGame/Assets/Scripts/Objects/AgentEntry.cs
+++ SpyGame/Assets/Scripts/Objects/AgentEntry.cs
@@ -4,4 +4,18 @@ using UnityEngine;
 
 public class AgentEntry
 {
+	public Sprite Portrait { get; private set; }
+	public int PlayerOwner { get; private set; }
+	public AgentTag Tag { get; private set; }
+	public AgentRank Rank { get; private set; }
+	public WorkRegion Region { get; private set; }
+
+	public AgentEntry(Sprite portrait, int playerOwner, AgentTag tag, AgentRank rank, WorkRegion region)
+	{
+		Portrait = portrait;
+		PlayerOwner = playerOwner;
+		Tag = tag;
+		Rank = rank;
+		Region = region;
+	}
 }
--- SpyGame/Assets/Scripts/Utils/Enums.cs
+++ SpyGame/Assets/Scripts/Utils/Enums.cs
@@ -1,3 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum WorkRegion
+{
+	Africa = 0,
+	Europe = 1,
+	USSR = 2,
+	Asia = 3,
+	America = 4,
+}
+
+public enum AgentTag
+{
+	None = -1,
+	Player1Spy = 0,
+	Player2Spy = 1,
+	Player3Spy = 2,
+	Player4Spy = 3,
+}
+
+public enum AgentRank
+{
+	None = -1,
+	RankOne = 0,
+	RankTwo = 1,
+	RankThree = 2,
+}
